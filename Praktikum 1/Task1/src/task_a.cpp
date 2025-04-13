6429193bd3570f4d5ec053b41e47f517951db02e
Majora <121319119+MajoraMoon@users.noreply.github.com>
2025-04-13 20:59:56 +0200

    can compile Botan on windows and linux with cmakefile

--- Praktikum 1/Task1/src/task_a.cpp
+++ Praktikum 1/Task1/src/task_a.cpp
@@ -7,11 +7,11 @@ void execute_task_1_a() {
   Botan::RSA_PrivateKey private_key(rng, 3000);
 
   std::cout << "Modulus (n): " << private_key.get_n() << "\n\n";
-  std::cout << "Öffentlicher Exponent (e): " << private_key.get_e() << "\n\n";
+  std::cout << "Oeffentlicher Exponent (e): " << private_key.get_e() << "\n\n";
   std::cout << "Privater Exponent (d): " << private_key.get_d() << "\n\n";
   std::cout << "Primzahl p: " << private_key.get_p() << "\n\n";
   std::cout << "Primzahl q: " << private_key.get_q() << "\n\n";
   std::cout << "d mod (p-1): " << private_key.get_d1() << "\n\n";
   std::cout << "d mod (q-1): " << private_key.get_d2() << "\n\n";
-  std::cout << "q⁻¹ mod p: " << private_key.get_c() << "\n";
+  std::cout << "q^-1 mod p: " << private_key.get_c() << "\n";
 }
